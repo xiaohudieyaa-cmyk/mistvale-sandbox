b5013a99a45b21de7cece410f90049b838597055
xiaohudieyaa-cmyk <xiaohudieyaa@gmail.com>
2026-07-15 14:47:49 +0800

    Add the game entry point

---
+++ src/main.tsx
@@ -0,0 +1,4 @@
+import { createRoot } from "react-dom/client";
+import MistvaleGame from "./MistvaleGame";
+import "./style.css";
+createRoot(document.getElementById("root")!).render(<MistvaleGame />);
